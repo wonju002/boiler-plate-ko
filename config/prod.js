e8c95662997901493bbbe62b691c8a4d1db2c5fc
juwon <juwon>
2024-01-05 14:39:49 +0900

    비밀 정보 보호

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURL: process.env.MONGO_URI //heroku에서 배포하는경우를 가정
+}
--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 5000
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+const { User } = require('./models/User');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true}));
+
+//application/json
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://emptycanmail01:nodejsPw01@boilerplate.3vftseu.mongodb.net/?retryWrites=true&w=majority', {
-  //useNewUrlParser:true
-}).then(()=>console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI, {
+  useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true, useFindAndModify:false
+})
+  .then(()=>console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello World!2434'))
+
+
+app.post('/register', (req, res) => {
+  //회원가입할때 필요한 정보들을 클라이언트에서 가져오면
+  //그것들을 데이터베이스에 넣어준다.
+
+  const user = new User(req.body)
+
+  user.save((err, userInfo)=>{
+    if(err) return res.json({success:false, err})
+
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`))
 
